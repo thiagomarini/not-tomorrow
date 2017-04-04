de5d40be2429853e379696277d3bac4dbd4111ac
Thiago Marini <thiago@weengs.co.uk>
2017-04-04 11:48:42 +0100

    Add PHPDoc to interface

--- src/NotTomorrow/Handlers/AnnualHolidays.php
+++ src/NotTomorrow/Handlers/AnnualHolidays.php
@@ -5,5 +5,12 @@ namespace NotTomorrow\Handlers;
 
 interface AnnualHolidays
 {
+    /**
+     * Should return an array of dates as string in the Y-m-d format:
+     * Example: ['2017-05-07','2018-01-07']
+     *
+     * @param int $year
+     * @return array
+     */
     public function getAll(int $year): array;
 }
